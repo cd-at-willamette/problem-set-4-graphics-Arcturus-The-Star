66c0aafebe4e182a4af86580c8858435065a57a4
Arcturus-The-Star <sjavery@willamette.edu>
2024-10-09 14:37:31 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -6,35 +6,41 @@
 
 from pgl import GWindow, GRect
 
-WIDTH = 400 #window width
-HEIGHT = 400 #window height
-BRICK_WIDTH = 40
-BRICK_HEIGHT = 20
-BRICKS_IN_BASE = 6
+WIDTH = 400             # The width of the graphics window
+HEIGHT = 400            # The height of the graphics window
+BRICK_WIDTH = 40        # The width of the bricks
+BRICK_HEIGHT = 20       # The height of the bricks
+BRICKS_IN_BASE = 6      # The number of bricks in the base of the pyramid
 
 def draw_pyramid():
     """ 
     Draws a pyramid of bricks centered on the screen with a height of BRICKS_IN_BASE. 
     """
 
-    gw = GWindow(WIDTH, HEIGHT) #sets up the graphics window
-    def make_brick(x:int, y:int): #the function to make a brick at coordinates (x,y)
-        rect = GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT) # make a brick of BRICK_WIDTH x BRICK_HEIGHT at (x,y)
-        rect.set_line_width(2) #makes the line width 2
-        gw.add(rect) #actually adds the rectangle
+    gw = GWindow(WIDTH, HEIGHT) # Sets up the graphics window
+    
+    def make_brick(x:int, y:int): # Makes a brick at coordinates (x,y)
+        rect = GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT) # Makes a brick of BRICK_WIDTH x BRICK_HEIGHT at (x,y)
+        rect.set_line_width(2)
+        gw.add(rect)
 
-    center_x = WIDTH // 2 #calculates the center of the window (x and y coordinates)
+    center_x = WIDTH // 2 # Calculates the center of the window (x and y coordinates)
     center_y = HEIGHT // 2
-    corner_x = center_x - ((BRICKS_IN_BASE * BRICK_WIDTH) // 2) #calculates the coordinates of the first brick (x and y)
+    
+    corner_x = center_x - ((BRICKS_IN_BASE * BRICK_WIDTH) // 2) # Calculates the coordinates of the first brick (x and y)
     corner_y = center_y + (((BRICKS_IN_BASE * BRICK_HEIGHT) // 2) - BRICK_HEIGHT)
-    row = 0
-    for row_num in range(BRICKS_IN_BASE):
-        for brick_num in range(BRICKS_IN_BASE - row):
+    
+    row = 0 # Tracks the row being created
+    
+    for row_num in range(BRICKS_IN_BASE): # Creates the pyramid, looping through one row at a time
+        
+        for brick_num in range(BRICKS_IN_BASE - row): # Creates a row of bricks, length dependent on the current row
             make_brick(corner_x, corner_y)
             corner_x += BRICK_WIDTH
-        corner_y -= BRICK_HEIGHT #brings the y coordinate up by one brick
+        
+        corner_y -= BRICK_HEIGHT # Brings the y coordinate up by one brick
         row += 1
-        corner_x = (center_x - ((BRICKS_IN_BASE * BRICK_WIDTH) // 2)) + ((BRICK_WIDTH // 2) * row) #resets the x coordinate of the brick to the proper location
+        corner_x = (center_x - ((BRICKS_IN_BASE * BRICK_WIDTH) // 2)) + ((BRICK_WIDTH // 2) * row) # Resets the x coordinate of the brick to the proper location
 
 
 if __name__ == '__main__':
